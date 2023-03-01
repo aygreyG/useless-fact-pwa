135e5b4efa6cd262256956720bf75b64d5262b62
Dávid Papp <78927537+aygreyG@users.noreply.github.com>
2023-03-01 18:22:37 +0100

    fix api endpoint

--- src/app/services/fact-api.service.ts
+++ src/app/services/fact-api.service.ts
@@ -7,20 +7,22 @@ import Fact from '../models/Fact';
 })
 export class FactApiService {
   public readonly factSubject$: Subject<Fact>;
+  private ENDPOINT =
+    'https://cors.gyergya.workers.dev/proxy/https://uselessfacts.jsph.pl/api/v2/facts';
 
   constructor() {
     this.factSubject$ = new Subject();
   }
 
   getRandomFact(): void {
-    fetch('https://uselessfacts.jsph.pl/random.json?language=en')
+    fetch(`${this.ENDPOINT}/random?language=en`)
       .then((res) => res.json())
       .then((data) => this.factSubject$.next(data))
       .catch((err) => console.error(err));
   }
 
   getToday(): void {
-    fetch('https://uselessfacts.jsph.pl/today.json?language=en')
+    fetch(`${this.ENDPOINT}/today?language=en`)
       .then((res) => res.json())
       .then((data) => this.factSubject$.next(data))
       .catch((err) => console.error(err));
